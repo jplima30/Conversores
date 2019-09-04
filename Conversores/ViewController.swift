a20ae65c1b8355e42c85b9f5afcfa823257b3cf5
João Silva <jpinfoagraria@gmail.com>
2019-09-03 21:43:34 -0300

    Conversor de peso/moeda/unidade
    Criação do conteúdo de assets, adaptação do lauchScreen e inicialização dos elementos do storyboard

---
+++ Conversores/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by jplima on 03/09/19.
+//  Copyright © 2019 jplima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+}
+
